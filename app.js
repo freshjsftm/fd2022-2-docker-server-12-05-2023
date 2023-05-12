7032a89425c6b1720cca3f8415e869099b227e79
Olena Zdanovska <freshjs.ftm@gmail.com>
2023-05-12 17:00:10 +0300

    Add Dockerfile

--- app.js
+++ app.js
@@ -1,8 +1,12 @@
 const express = require('express');
 const app = express();
 
-app.use((req, res) => {
-  res.send('Hello from container');
+app.get('/', (req, res) => {
+  res.send('Hi!');
 });
+
+app.get('/about', (req, res)=>{
+  res.send('About us!')
+})
 
 module.exports = app;
